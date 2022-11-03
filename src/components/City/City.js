f5b469ccfcb6d8c70ba2d5847ac792c9d216f816
vitor-fontenele <vitor_fontenele@hotmail.com>
2022-11-03 19:47:21 -0300

    Add city picker section

---
+++ src/components/City/City.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+export default function City(props){ 
+    const conditionToPT = (condition) => {
+        const translatedConditions = {
+            "Clouds": "Nuvens",
+            "Clear": "Claro",
+            "Drizzle": "Garoa",
+            "Thunderstorm": "Tempestade",
+            "Atmosphere": "Névoa",
+            "Snow": "Neve",
+            "Rain": "Chuva",
+            "Mist": "Névoa",
+            "Smoke": "Fumaça",
+            "Haze": "Confusão",
+            "Dust": "Pó",
+            "Fog": "Névoa",
+            "Sand": "Areia",
+            "Dust": "Pó",
+            "Ash": "Cinza",
+            "Squall": "Tempestade",
+            "Tornado": "Tornado"
+        }
+        return translatedConditions[condition];
+    }
+    
+    const formatTemperature = (temperature) => {
+        return Math.round(temperature - 273);
+    }
+    
+    return(
+        <div id="special-card" className="card">
+            <h3 className="card-city">{props.brCityData["name"]} <span className="card-state"></span></h3>
+            <div className="card-info">
+                <div className="card-image-info">
+                    <img className="card-image" src={`http://openweathermap.org/img/wn/${props.brCityData["weather"][0]["icon"]}@2x.png`} alt="" />
+                </div>
+                <div className="card-text-info">
+                    <h4 className="card-temperature">{`${formatTemperature(props.brCityData["main"]["temp"])}ºC`}</h4>
+                    <h5 className="card-condition">{conditionToPT(props.brCityData["weather"][0]["main"])}</h5>
+                    <h6 className="card-max-min">{`Umidade: ${props.brCityData["main"]["humidity"]}%`}</h6>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/components/CityPicker/CityPicker.js
+++ src/components/CityPicker/CityPicker.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import City from '../City/City';
 import './styled.css';
 
 export default function CityPicker(props){
     const handleBrState = (event) => {
         props.setBrState(event.target.value);
-        //console.log(brState);
-        // console.log(props.statesInfo);
-      }
+    }
     
-      const handleBrCity = async (event) => {
+    const handleBrCity = (event) => {
         props.setBrCity(event.target.value);
-        // console.log(event.target.id);
-        const queryId = "";
+    }
+
+    const handleClick = async () => {
+        const cityData = props.citiesInfo
+            .filter(city => city["state"] === props.brState)
+            .filter(city => city["name"] === props.brCity);
+        const queryId = cityData[0]["id"];
         const url = `http://api.openweathermap.org/data/2.5/group?id=${queryId}&units;=metric&appid=fe772bb5ff9d8486d890ff783f7fcf86`;
         let data;
         try {
@@ -20,26 +24,31 @@ export default function CityPicker(props){
         } catch {
             console.log("Erro ao mostrar condições em cidade.");
         }
-        console.log(data);
+        // console.log(data);
         // props.setBrCity(event.target.value);
         props.setBrCityData({...data["list"][0]})
-      }
+    }  
     
     return (
         <section id="city-picker-section">
-            <select onChange={handleBrState} value={props.brState}>
-                {props.statesInfo.map(state => {
-                    return <option value={state.name}>{state.name}</option>
-                })}
-            </select>
-            <select onChange={handleBrCity} value={props.brCity}>
-                {props.citiesInfo
-                .filter(city => city["state"] === props.brState)
-                .map(city => {
-                    return <option value={city.name}>{city.name}</option>
-                })}
-            </select>
-            <h1>{props.brCityData["main"] ? props.brCityData["main"]["temp"] : ""}</h1>
+            <div id="city-picker-content">
+                <h2 id="city-picker-title">Escolha uma cidade de qualquer estado</h2>
+                <select className="element" onChange={handleBrState} value={props.brState}>
+                    {props.statesInfo.map(state => {
+                        return <option value={state.name}>{state.name}</option>
+                    })}
+                </select>
+                <select className="element" onChange={handleBrCity} value={props.brCity}>
+                    {props.citiesInfo
+                    .filter(city => city["state"] === props.brState)
+                    .map(city => {
+                        return <option value={city.name}>{city.name}</option>
+                    })}
+                </select>
+                <button className="element" onClick={handleClick}>Ver dados</button>
+                {props.brCityData["main"] ? 
+                    <City brCityData={props.brCityData}/> : <></>}
+            </div>
         </section>
     )
 }
